543feec9b2475e2c3889906f617d4a4b191f1a31
Paulo Rodriguez Contreras <prodriguezc@unsa.edu.pe>
2020-06-15 23:16:48 -0500

    MVC idea of architecture

---
+++ DNPA_DepressionSafetyTracking/app/src/main/java/com/dnpa/lab07_determineorientation/Controller/MVC_Controller.java
@@ -0,0 +1,5 @@
+package com.dnpa.lab07_determineorientation.Controller;
+
+public interface MVC_Controller {
+    public void onViewLoaded();
+}
---
+++ DNPA_DepressionSafetyTracking/app/src/main/java/com/dnpa/lab07_determineorientation/View/MVC_View.java
@@ -0,0 +1,11 @@
+package com.dnpa.lab07_determineorientation.View;
+
+import android.view.View;
+import java.util.List;
+
+public interface MVC_View {
+
+    public View getRootView();
+    public void initViews();
+    public void bindDataToView();
+}
