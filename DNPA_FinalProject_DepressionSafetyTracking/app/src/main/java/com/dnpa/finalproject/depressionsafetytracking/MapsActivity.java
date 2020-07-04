7ad6f13b8d7d890a7d166177404a0a47a2ff2b4b
Paulo Rodriguez Contreras <prodriguezc@unsa.edu.pe>
2020-07-04 18:47:01 -0500

    testing acelerometer reading data in project

--- DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/MapsActivity.java
+++ DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/MapsActivity.java
@@ -5,7 +5,6 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -33,7 +32,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.maps_activity);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
