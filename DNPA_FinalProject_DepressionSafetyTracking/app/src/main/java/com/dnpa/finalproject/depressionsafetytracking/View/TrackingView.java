9e3bc31f864ef53f3c54a5ba6a5722329d1a0efd
Paulo Rodriguez Contreras <prodriguezc@unsa.edu.pe>
2020-08-03 20:57:44 -0500

    Navigation Bar and Menu options integration

--- DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/View/TrackingView.java
+++ DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/View/TrackingView.java
@@ -1,23 +1,36 @@
 package com.dnpa.finalproject.depressionsafetytracking.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.dnpa.finalproject.depressionsafetytracking.Location.LocationReceiver;
@@ -28,8 +41,9 @@ import com.dnpa.finalproject.depressionsafetytracking.Presenter.TrackingPresente
 import com.dnpa.finalproject.depressionsafetytracking.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.material.navigation.NavigationView;
 
-public class TrackingView extends AppCompatActivity implements ITrackingView, View.OnClickListener{
+public class TrackingView extends AppCompatActivity implements ITrackingView, View.OnClickListener, NavigationView.OnNavigationItemSelectedListener{
 
     private int MY_PERMISSIONS_REQUEST_READ_CONTACTS;
     private ITrackingPresenter presenter;
@@ -48,6 +62,19 @@ public class TrackingView extends AppCompatActivity implements ITrackingView, Vi
     private TextView sensorYValue;
     private TextView sensorZValue;
     private SensorManager sensorManager;
+
+    //Actualizar en Navigation Drawer
+    NavigationView navigationView;
+    TextView nav_user;
+    TextView nav_mail;
+    ImageView nav_image;
+    private DrawerLayout drawer;
+
+    //Dialog about
+    Dialog myDialog;
+    ImageView closeAboutImg;
+    Button btnAccept;
+    TextView titleTv, messageTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +115,39 @@ public class TrackingView extends AppCompatActivity implements ITrackingView, Vi
         if (!hasPermissions(this, PERMISSIONS)) {
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         }
+
+        //NAV BAR
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View hView = navigationView.getHeaderView(0);
+        nav_user = (TextView) hView.findViewById(R.id.name_user);
+        nav_mail = (TextView) hView.findViewById(R.id.mail_user);
+        nav_image = (ImageView) hView.findViewById(R.id.image_user);
+
+        if(savedInstanceState == null){
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+        myDialog = new Dialog(this);
+
+        //Datos de usuario
+        //Navigation
+        /*
+        nav_user.setText(personName);
+        nav_mail.setText(personEmail);
+        Glide.with(this).load(personPhoto).into(nav_image);
+         */
     }
 
     @Override
@@ -193,5 +253,87 @@ public class TrackingView extends AppCompatActivity implements ITrackingView, Vi
                 });
         alert = builder.create();
         alert.show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            case R.id.nav_ajustes:
+                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AjustesFragment()).commit();
+                break;
+            case R.id.nav_about:
+                showAbout();
+                break;
+            case R.id.nav_salir:
+                new AlertDialog.Builder(this)
+                        .setTitle("Confirmación de Salida")
+                        .setMessage("Esta seguro de que desea salir de su sesion")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //signOut();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ajustes:
+                Toast.makeText(this, "aJUSTES", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.acerca:
+                Toast.makeText(this, "Acerca", Toast.LENGTH_SHORT).show();
+                //showAbout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Mostrar CUSTOM DIALOG BOX
+    public void showAbout(){
+        myDialog.setContentView(R.layout.about);
+        closeAboutImg = (ImageView) myDialog.findViewById(R.id.closeAbout);
+        btnAccept = (Button) myDialog.findViewById(R.id.btnAceptar);
+        titleTv = (TextView) myDialog.findViewById(R.id.titleAbout);
+        messageTv = (TextView) myDialog.findViewById(R.id.messageAbout);
+
+        btnAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+
+        closeAboutImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
