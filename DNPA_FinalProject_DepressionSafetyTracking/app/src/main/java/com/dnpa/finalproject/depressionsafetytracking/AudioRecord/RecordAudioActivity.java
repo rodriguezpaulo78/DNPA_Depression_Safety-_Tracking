bf37f5ebfd2b3e47b0ba2643abf7611460a861ee
Paulo Rodriguez Contreras <prodriguezc@unsa.edu.pe>
2020-08-07 15:04:12 -0500

    Saving audio recordings into a .mp3 and .wav files correctly

--- DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/AudioRecord/RecordAudioActivity.java
+++ DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/AudioRecord/RecordAudioActivity.java
@@ -27,11 +27,15 @@ import net.sourceforge.lame.mp3.Lame;
 import net.sourceforge.lame.mp3.MPEGMode;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -186,6 +190,7 @@ public class RecordAudioActivity extends AppCompatActivity {
         }
 
 
+
         try {
             os.close();
         } catch (IOException e) {
@@ -221,7 +226,20 @@ public class RecordAudioActivity extends AppCompatActivity {
         try {
             in = new FileInputStream( file );
             in.read( byteData );
+
+
             encodePcmToMp3(byteData);
+
+
+            ///
+            File f1 = new File("/sdcard/PORFIN.pcm"); // The location of your PCM file
+            File f2 = new File("/sdcard/PORFIN.wav"); // The location where you want your WAV file
+            try {
+                rawToWave(f1, f2);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             in.close();
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
@@ -250,7 +268,7 @@ public class RecordAudioActivity extends AppCompatActivity {
 
         //fast  gmm2  LameEncoder encoder = new LameEncoder(new javax.sound.sampled.AudioFormat(88200.0f, 16, 2, true, false), 256, MPEGMode.STEREO, Lame.QUALITY_HIGHEST, false);
         Log.d(TAG, "laptm3" + (int) pcm.length+"");
-        LameEncoder encoder = new LameEncoder(new javax.sound.sampled.AudioFormat(44100.0f, 16, 2, true, false), 256, MPEGMode.STEREO, Lame.QUALITY_HIGHEST, false);
+        LameEncoder encoder = new LameEncoder(new javax.sound.sampled.AudioFormat(8000.0f, 16, 1, true, false), 256, MPEGMode.STEREO, Lame.QUALITY_HIGHEST, false);
 
         ByteArrayOutputStream mp3 = new ByteArrayOutputStream();
         byte[] buffer = new byte[encoder.getPCMBufferSize()];
@@ -268,7 +286,7 @@ public class RecordAudioActivity extends AppCompatActivity {
 
         encoder.close();
 
-        File file = new File("/sdcard/wawa.mp3");
+        File file = new File("/sdcard/PORFIN.mp3");
         if (!file.exists()) {
             try {
                 file.createNewFile();
@@ -279,7 +297,7 @@ public class RecordAudioActivity extends AppCompatActivity {
 
             FileOutputStream stream = null;
             try {
-                stream = new FileOutputStream("/sdcard/wawa.mp3");
+                stream = new FileOutputStream("/sdcard/PORFIN.mp3");
                 stream.write(mp3.toByteArray());
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -289,6 +307,96 @@ public class RecordAudioActivity extends AppCompatActivity {
 
         }
         //   return mp3.toByteArray();
+    }
+
+
+
+    private void rawToWave(final File rawFile, final File waveFile) throws IOException {
+
+        byte[] rawData = new byte[(int) rawFile.length()];
+        DataInputStream input = null;
+        try {
+            input = new DataInputStream(new FileInputStream(rawFile));
+            input.read(rawData);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
+
+        DataOutputStream output = null;
+        try {
+            output = new DataOutputStream(new FileOutputStream(waveFile));
+            // WAVE header
+            // see http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+            writeString(output, "RIFF"); // chunk id
+            writeInt(output, 36 + rawData.length); // chunk size
+            writeString(output, "WAVE"); // format
+            writeString(output, "fmt "); // subchunk 1 id
+            writeInt(output, 16); // subchunk 1 size
+            writeShort(output, (short) 1); // audio format (1 = PCM)
+            writeShort(output, (short) 1); // number of channels
+            writeInt(output, 8000); // sample rate
+            writeInt(output, RECORDER_SAMPLERATE * 2); // byte rate
+            writeShort(output, (short) 2); // block align
+            writeShort(output, (short) 16); // bits per sample
+            writeString(output, "data"); // subchunk 2 id
+            writeInt(output, rawData.length); // subchunk 2 size
+            // Audio data (conversion big endian -> little endian)
+            short[] shorts = new short[rawData.length / 2];
+            ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
+            ByteBuffer bytes = ByteBuffer.allocate(shorts.length * 2);
+            for (short s : shorts) {
+                bytes.putShort(s);
+            }
+
+            output.write(fullyReadFileToBytes(rawFile));
+        } finally {
+            if (output != null) {
+                output.close();
+            }
+        }
+    }
+    byte[] fullyReadFileToBytes(File f) throws IOException {
+        int size = (int) f.length();
+        byte bytes[] = new byte[size];
+        byte tmpBuff[] = new byte[size];
+        FileInputStream fis= new FileInputStream(f);
+        try {
+
+            int read = fis.read(bytes, 0, size);
+            if (read < size) {
+                int remain = size - read;
+                while (remain > 0) {
+                    read = fis.read(tmpBuff, 0, remain);
+                    System.arraycopy(tmpBuff, 0, bytes, size - remain, read);
+                    remain -= read;
+                }
+            }
+        }  catch (IOException e){
+            throw e;
+        } finally {
+            fis.close();
+        }
+
+        return bytes;
+    }
+    private void writeInt(final DataOutputStream output, final int value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+        output.write(value >> 16);
+        output.write(value >> 24);
+    }
+
+    private void writeShort(final DataOutputStream output, final short value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+    }
+
+    private void writeString(final DataOutputStream output, final String value) throws IOException {
+        for (int i = 0; i < value.length(); i++) {
+            output.write(value.charAt(i));
+        }
     }
 
 }
