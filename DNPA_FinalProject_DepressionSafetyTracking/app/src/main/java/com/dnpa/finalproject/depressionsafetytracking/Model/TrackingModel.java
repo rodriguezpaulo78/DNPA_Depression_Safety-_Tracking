b583d47047ad3fe5d20a8a65da87298018f6c587
Paulo Rodriguez Contreras <prodriguezc@unsa.edu.pe>
2020-08-10 16:54:12 -0500

    OrientationEventListener created to handle orientation data separated from Model

--- DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/Model/TrackingModel.java
+++ DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/Model/TrackingModel.java
@@ -2,12 +2,11 @@ package com.dnpa.finalproject.depressionsafetytracking.Model;
 
 import android.annotation.SuppressLint;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
+import com.dnpa.finalproject.depressionsafetytracking.Orientation.OrientationEventListener;
 import com.dnpa.finalproject.depressionsafetytracking.Presenter.ITrackingPresenter;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -16,7 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TrackingModel implements ITrackingModel, SensorEventListener {
+public class TrackingModel implements ITrackingModel {
 
     private ITrackingPresenter presenter;
 
@@ -25,16 +24,9 @@ public class TrackingModel implements ITrackingModel, SensorEventListener {
     Map<String,Object> dbValues = new HashMap<>();
 
     //ORIENTATION HANDLING
-    private static final String TAG = "Model";
     private static final int RATE = SensorManager.SENSOR_DELAY_NORMAL;
-    private float[] accelerationValues;
-    private float[] magneticValues;
-    private boolean isFaceUp;
-    String x=null,y=null,z=null,orientation=null;
-    private boolean readingAccelerationData = false;
-
     public static String user;
-    String ZValue, YValue, XValue;
+    private OrientationEventListener orientationListener;
 
     public TrackingModel(ITrackingPresenter presenter){
         this.presenter = presenter;
@@ -45,127 +37,16 @@ public class TrackingModel implements ITrackingModel, SensorEventListener {
             user = a;
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        float[] rotationMatrix;
-
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_ACCELEROMETER:
-                accelerationValues = event.values.clone();
-                rotationMatrix = generateRotationMatrix();
-                if (rotationMatrix != null) {
-                    determineOrientation(rotationMatrix);
-                }
-                break;
-
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                magneticValues = event.values.clone();
-                rotationMatrix = generateRotationMatrix();
-                if (rotationMatrix != null) {
-                    determineOrientation(rotationMatrix);
-                }
-                break;
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        Log.d(TAG, String.format("Accuracy for sensor %s = %d", sensor.getName(), accuracy));
-    }
-
-    /**
-     * Genera una matriz de rotación a partir de los valores de los sensores
-     */
-    @SuppressLint("LongLogTag")
-    private float[] generateRotationMatrix() {
-        float[] rotationMatrix = null;
-
-        if (accelerationValues != null && magneticValues != null) {
-            rotationMatrix = new float[16];
-            boolean rotationMatrixGenerated;
-            rotationMatrixGenerated =
-                    SensorManager.getRotationMatrix(rotationMatrix,
-                            null, accelerationValues, magneticValues);
-            if (!rotationMatrixGenerated) {
-                Log.w(TAG, "Failed to generate rotation matrix");
-                rotationMatrix = null;
-            }
-        }
-        return rotationMatrix;
-    }
-
-    /**
-     * Usa los valores de los sensores para determinar la orientación del dispositivo
-     * asi como también mostrar tales valores en la interfaz  y guardarlos en la BD
-     * @param rotationMatrix
-     */
-    private void determineOrientation(float[] rotationMatrix) {
-        float[] orientationValues = new float[3];
-        SensorManager.getOrientation(rotationMatrix, orientationValues);
-
-        double azimuth = Math.toDegrees(orientationValues[0]);
-        double pitch = Math.toDegrees(orientationValues[1]);
-        double roll = Math.toDegrees(orientationValues[2]);
-        ZValue = String.valueOf(azimuth);
-        XValue = String.valueOf(pitch);
-        YValue = String.valueOf(roll);
-
-        x=(String.valueOf(azimuth));
-        y=(String.valueOf(pitch));
-        z=(String.valueOf(roll));
-
-        Map<String,Object> xyzValues = new HashMap<>();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        //Para ver si esta de cabeza o de frente
-        if (pitch <= 10) {
-            if (Math.abs(roll) >= 170) {
-                onFaceDown();
-                writeData();
-            } else if (Math.abs(roll) <= 10) {
-                onFaceUp();
-                writeData( );
-            }
-        }
-    }
-
-    @SuppressLint("LongLogTag")
-    private void writeData(){
-        Log.d(TAG, "Valores = (" + ZValue + ", " + XValue + ", " +YValue +")");
-        //Guarda los valores en la BD
-        dbValues.put("x (orient)", XValue);
-        dbValues.put("y (orient)", YValue);
-        dbValues.put("z (orient)", ZValue);
-        mDatabase.child(user).push().setValue(dbValues);
-        //mDatabase.removeValue(); //método para eliminar si hay muchos datos en BD
-
-        //Llama al método del presentador para actualizar los valores
-        presenter.showData(XValue,YValue,ZValue,orientation);
-    }
-
-    private void onFaceUp() {
-        if (!isFaceUp) {
-            orientation=("Face up");
-            isFaceUp = true;
-        }
-    }
-
-    private void onFaceDown() {
-        if (isFaceUp) {
-            orientation=("Face down");
-            isFaceUp = false;
-        }
-    }
-
     /**
      * Actualiza los valores del sensor al iniciar monitoreo
      */
     public void updateSelectedSensor(SensorManager sensorManager) {
         // Limpiar algun registro realizado anteriormente
-        sensorManager.unregisterListener(this);
-        sensorManager.registerListener(this,
+        orientationListener = new OrientationEventListener(presenter, user);
+        sensorManager.unregisterListener(orientationListener);
+        sensorManager.registerListener(orientationListener,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), RATE);
-        sensorManager.registerListener(this,
+        sensorManager.registerListener(orientationListener,
                 sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), RATE);
     }
 
@@ -174,7 +55,7 @@ public class TrackingModel implements ITrackingModel, SensorEventListener {
      */
     public void stopSelectedSensor(SensorManager sensorManager) {
         // Limpiar algun registro realizado anteriormente
-        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(orientationListener);
 
         //Llama al método del presentador para actualizar los valores
         presenter.showData("","","","");
--- DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/Orientation/OrientationEventListener.java
+++ DNPA_FinalProject_DepressionSafetyTracking/app/src/main/java/com/dnpa/finalproject/depressionsafetytracking/Orientation/OrientationEventListener.java
@@ -1,11 +1,150 @@
 package com.dnpa.finalproject.depressionsafetytracking.Orientation;
 
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
 import com.androidplot.xy.SimpleXYSeries;
+import com.dnpa.finalproject.depressionsafetytracking.Presenter.ITrackingPresenter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //Esta clase se encargará del manejo de la lectura de orientación  (acelerómetro)
 // por parte del usuario separada de la lógica principal de monitoreo
-public class OrientationEventListener {
-    private String xSeries;
-    private String ySeries;
-    private String zSeries;
+public class OrientationEventListener implements SensorEventListener {
+    private static final String TAG = "Model";
+    private ITrackingPresenter presenter;
+    String user;
+    private float[] accelerationValues;
+    private float[] magneticValues;
+    String ZValue, YValue, XValue;
+    private boolean isFaceUp;
+    String x=null,y=null,z=null,orientation=null;
+
+    //FIREBASE
+    DatabaseReference mDatabase;
+    Map<String,Object> dbValues = new HashMap<>();
+
+    public OrientationEventListener(ITrackingPresenter presenter, String user){
+        this.presenter = presenter;
+        this.user = user;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] rotationMatrix;
+
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                accelerationValues = event.values.clone();
+                rotationMatrix = generateRotationMatrix();
+                if (rotationMatrix != null) {
+                    determineOrientation(rotationMatrix);
+                }
+                break;
+
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                magneticValues = event.values.clone();
+                rotationMatrix = generateRotationMatrix();
+                if (rotationMatrix != null) {
+                    determineOrientation(rotationMatrix);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, String.format("Accuracy for sensor %s = %d", sensor.getName(), accuracy));
+    }
+
+    /**
+     * Genera una matriz de rotación a partir de los valores de los sensores
+     */
+    @SuppressLint("LongLogTag")
+    private float[] generateRotationMatrix() {
+        float[] rotationMatrix = null;
+
+        if (accelerationValues != null && magneticValues != null) {
+            rotationMatrix = new float[16];
+            boolean rotationMatrixGenerated;
+            rotationMatrixGenerated =
+                    SensorManager.getRotationMatrix(rotationMatrix,
+                            null, accelerationValues, magneticValues);
+            if (!rotationMatrixGenerated) {
+                Log.w(TAG, "Failed to generate rotation matrix");
+                rotationMatrix = null;
+            }
+        }
+        return rotationMatrix;
+    }
+
+    /**
+     * Usa los valores de los sensores para determinar la orientación del dispositivo
+     * asi como también mostrar tales valores en la interfaz  y guardarlos en la BD
+     * @param rotationMatrix
+     */
+    private void determineOrientation(float[] rotationMatrix) {
+        float[] orientationValues = new float[3];
+        SensorManager.getOrientation(rotationMatrix, orientationValues);
+
+        double azimuth = Math.toDegrees(orientationValues[0]);
+        double pitch = Math.toDegrees(orientationValues[1]);
+        double roll = Math.toDegrees(orientationValues[2]);
+        ZValue = String.valueOf(azimuth);
+        XValue = String.valueOf(pitch);
+        YValue = String.valueOf(roll);
+
+        x=(String.valueOf(azimuth));
+        y=(String.valueOf(pitch));
+        z=(String.valueOf(roll));
+
+        Map<String,Object> xyzValues = new HashMap<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        //Para ver si esta de cabeza o de frente
+        if (pitch <= 10) {
+            if (Math.abs(roll) >= 170) {
+                onFaceDown();
+                writeData();
+            } else if (Math.abs(roll) <= 10) {
+                onFaceUp();
+                writeData( );
+            }
+        }
+    }
+
+    @SuppressLint("LongLogTag")
+    private void writeData(){
+        Log.d(TAG, "Valores = (" + ZValue + ", " + XValue + ", " +YValue +")");
+        //Guarda los valores en la BD
+        dbValues.put("x (orient)", XValue);
+        dbValues.put("y (orient)", YValue);
+        dbValues.put("z (orient)", ZValue);
+        mDatabase.child(user).push().setValue(dbValues);
+        //mDatabase.removeValue(); //método para eliminar si hay muchos datos en BD
+
+        //Llama al método del presentador para actualizar los valores
+        presenter.showData(XValue,YValue,ZValue,orientation);
+    }
+
+    private void onFaceUp() {
+        if (!isFaceUp) {
+            orientation=("Face up");
+            isFaceUp = true;
+        }
+    }
+
+    private void onFaceDown() {
+        if (isFaceUp) {
+            orientation=("Face down");
+            isFaceUp = false;
+        }
+    }
 }
